3978cf36554d1c47fe6107c84f3ecded5cfc9eb9
Daniel Rama <daniel.morais.rama@gmail.com>
2025-05-02 15:25:56 +0100

    Teste commit

---
+++ cmake-build-debug/CMakeFiles/tester.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tester.dir/main/Tester.cpp.obj"
+  "CMakeFiles/tester.dir/main/Tester.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Color.cpp.obj"
+  "CMakeFiles/tester.dir/src/Color.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Command.cpp.obj"
+  "CMakeFiles/tester.dir/src/Command.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Command/Blank.cpp.obj"
+  "CMakeFiles/tester.dir/src/Command/Blank.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Command/Open.cpp.obj"
+  "CMakeFiles/tester.dir/src/Command/Open.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Command/Save.cpp.obj"
+  "CMakeFiles/tester.dir/src/Command/Save.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Image.cpp.obj"
+  "CMakeFiles/tester.dir/src/Image.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Logger.cpp.obj"
+  "CMakeFiles/tester.dir/src/Logger.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/PNG.cpp.obj"
+  "CMakeFiles/tester.dir/src/PNG.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Scrim.cpp.obj"
+  "CMakeFiles/tester.dir/src/Scrim.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/ScrimParser.cpp.obj"
+  "CMakeFiles/tester.dir/src/ScrimParser.cpp.obj.d"
+  "CMakeFiles/tester.dir/src/Utils.cpp.obj"
+  "CMakeFiles/tester.dir/src/Utils.cpp.obj.d"
+  "libtester.dll.a"
+  "tester.exe"
+  "tester.exe.manifest"
+  "tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
