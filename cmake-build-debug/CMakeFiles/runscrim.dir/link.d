ad13b8740620a19ee613970b29e5b9e43bcf851e
danielramaa <daniel.morais.rama@gmail.com>
2025-05-23 22:25:26 +0100

    FINAL VERSION.

---
+++ cmake-build-debug/CMakeFiles/runscrim.dir/link.d
@@ -0,0 +1,187 @@
+runscrim: \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libasan_preinit.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libasan.so \
+  CMakeFiles/runscrim.dir/src/Color.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Add.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Chain.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Crop.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Fill.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Invert.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Mirror.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Move.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Open.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Replace.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Resize.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Rotate.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Rotate_left.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Rotate_right.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Save.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Scaleup.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/Slide.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/h_mirror.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/to_gray_scale.cpp.o \
+  CMakeFiles/runscrim.dir/src/Command/v_mirror.cpp.o \
+  CMakeFiles/runscrim.dir/src/Image.cpp.o \
+  CMakeFiles/runscrim.dir/src/Logger.cpp.o \
+  CMakeFiles/runscrim.dir/src/PNG.cpp.o \
+  CMakeFiles/runscrim.dir/src/Scrim.cpp.o \
+  CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o \
+  CMakeFiles/runscrim.dir/src/Utils.cpp.o \
+  CMakeFiles/runscrim.dir/main/RunScrim.cpp.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libubsan.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libasan_preinit.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libasan.so:
+
+CMakeFiles/runscrim.dir/src/Color.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Add.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Blank.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Chain.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Crop.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Fill.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Invert.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Mirror.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Move.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Open.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Replace.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Resize.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Rotate.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Rotate_left.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Rotate_right.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Save.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Scaleup.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/Slide.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/h_mirror.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/to_gray_scale.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Command/v_mirror.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Image.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Logger.cpp.o:
+
+CMakeFiles/runscrim.dir/src/PNG.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Scrim.cpp.o:
+
+CMakeFiles/runscrim.dir/src/ScrimParser.cpp.o:
+
+CMakeFiles/runscrim.dir/src/Utils.cpp.o:
+
+CMakeFiles/runscrim.dir/main/RunScrim.cpp.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libubsan.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o:
+
+/lib64/ld-linux-x86-64.so.2:
---
+++ cmake-build-debug/CMakeFiles/tester.dir/link.d
@@ -0,0 +1,187 @@
+tester: \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libasan_preinit.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libasan.so \
+  CMakeFiles/tester.dir/src/Color.cpp.o \
+  CMakeFiles/tester.dir/src/Command.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Add.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Blank.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Chain.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Crop.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Fill.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Invert.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Mirror.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Move.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Open.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Replace.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Resize.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Rotate.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Rotate_left.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Rotate_right.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Save.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Scaleup.cpp.o \
+  CMakeFiles/tester.dir/src/Command/Slide.cpp.o \
+  CMakeFiles/tester.dir/src/Command/h_mirror.cpp.o \
+  CMakeFiles/tester.dir/src/Command/to_gray_scale.cpp.o \
+  CMakeFiles/tester.dir/src/Command/v_mirror.cpp.o \
+  CMakeFiles/tester.dir/src/Image.cpp.o \
+  CMakeFiles/tester.dir/src/Logger.cpp.o \
+  CMakeFiles/tester.dir/src/PNG.cpp.o \
+  CMakeFiles/tester.dir/src/Scrim.cpp.o \
+  CMakeFiles/tester.dir/src/ScrimParser.cpp.o \
+  CMakeFiles/tester.dir/src/Utils.cpp.o \
+  CMakeFiles/tester.dir/main/Tester.cpp.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libubsan.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libasan_preinit.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libasan.so:
+
+CMakeFiles/tester.dir/src/Color.cpp.o:
+
+CMakeFiles/tester.dir/src/Command.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Add.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Blank.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Chain.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Crop.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Fill.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Invert.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Mirror.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Move.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Open.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Replace.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Resize.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Rotate.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Rotate_left.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Rotate_right.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Save.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Scaleup.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/Slide.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/h_mirror.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/to_gray_scale.cpp.o:
+
+CMakeFiles/tester.dir/src/Command/v_mirror.cpp.o:
+
+CMakeFiles/tester.dir/src/Image.cpp.o:
+
+CMakeFiles/tester.dir/src/Logger.cpp.o:
+
+CMakeFiles/tester.dir/src/PNG.cpp.o:
+
+CMakeFiles/tester.dir/src/Scrim.cpp.o:
+
+CMakeFiles/tester.dir/src/ScrimParser.cpp.o:
+
+CMakeFiles/tester.dir/src/Utils.cpp.o:
+
+CMakeFiles/tester.dir/main/Tester.cpp.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libubsan.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o:
+
+/lib64/ld-linux-x86-64.so.2:
